2896d4466a03cf10f36bc8cf943b3c6f72c0d731
fpankretic <filip.bonbon@gmail.com>
2023-09-05 00:25:06 +0200

    update .gitignore

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 git pull
 mvn clean package
 clear
-mvn -q exec:java -Dexec.mainClass="MainKt" -Dexec.args="MTEwOTQ5NDQwOTIxNDU2NjUwMA.GUVQv1.GgvxRMUHVceyv7-xWdxpgi2F7Pv8LiHlUpMcFg"
+mvn -q exec:java -Dexec.mainClass="MainKt" -Dexec.args="MTEwOTQ5NDQwOTIxNDU2NjUwMA.GUVQv1.GgvxRMUHVceyv7-xWdxpgi2F7Pv8LiHlUpMcFg
