bfebe165f7610a5c7dcf468c3e9fb9950281af1e
fpankretic <filip.bonbon@gmail.com>
2023-09-17 21:02:53 +0200

    commit to master, remove all println loggers :)

--- src/main/kotlin/handler/AllEventHandler.kt
+++ src/main/kotlin/handler/AllEventHandler.kt
@@ -1,13 +1,16 @@
 package handler
 
 import discord4j.core.event.domain.Event
+import mu.KotlinLogging
 import reactor.core.publisher.Mono
 
 class AllEventHandler {
 
+    private val logger = KotlinLogging.logger {}
+
     fun handle(event: Event): Mono<Void> {
         return Mono.justOrEmpty(event)
-            .map { println(it.javaClass.name) }
+            .map { logger.info { it.javaClass.name } }
             .then()
     }
 
